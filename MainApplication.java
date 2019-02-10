e4c1372578a8fd3c7f76457355ce963556c87b03
Swapna Panchomarthi <swapnapanchomarthi@zipcoders-mbp.lan>
2019-02-09 22:52:17 -0500

    Swapna lab - core calculator and test cases

--- MainApplication.java
+++ MainApplication.java
@@ -1,4 +1,141 @@
+
+import java.util.Scanner;
+import java.util.Random;
 public class MainApplication {
-    public static void main(String[] args) {
+    
+    private static Scanner scanner = new Scanner(System.in);
+    private static String operator = "";
+    static int numberOfInputs = 0;
+    static double[] inputs = new double[10];
+    static String s = "Error";
+    
+    private static double ask(Scanner scanner, String prompt) {
+        System.out.print(prompt);
+        return scanner.nextDouble();}
+   
+    
+    public void display(double first,String operator, double second, double result ){
+        System.out.printf("%.1f %c %.1f = %.1f", first, operator, second, result);
     }
+  
+public static void main(String[] args) {
+    MainApplication ma = new MainApplication();
+    String s = "Error";
+    System.out.println("Please input an operator from below");
+    System.out.println("+ "+"- "+"* "+"/ "+"^ "+"sq  "+"sqrt "+"1/x "+"+or-");
+    operator = scanner.next();
+    double answer = 0;
+    System.out.println(operator);
+    if (operator.equals("+") || operator.equals("-")||operator.equals("*") ||operator.equals("/") ||operator.equals("^")) {
+        numberOfInputs = 2;
+    }
+   
+    if (operator.equals("sq")||operator.equals("sqrt")||operator.equals("1/x")||operator.equals("+or-")) {
+        numberOfInputs = 1;
+     
+    }
+    
+    
+    for (int i = 0; i < numberOfInputs; i++){
+        inputs[i] = ask(scanner, "Please enter next input");
+    }
+    
+    if (operator.equals("+")) {
+        answer = sum(inputs[0], inputs[1]);
+    }
+    
+    if (operator.equals("-")) {
+        answer = difference(inputs[0], inputs[1]);
+    }
+    
+    if (operator.equals("*")) {
+        answer = product(inputs[0], inputs[1]);
+    }
+    
+    if (operator.equals("/")) {
+        answer = division(inputs[0], inputs[1]);
+    }
+    
+    if (operator.equals("sq")) {
+        answer = square(inputs[0]);
+    }
+    
+    if (operator.equals("sqrt")) {
+        answer = squareroot(inputs[0]);
+    }
+    
+    if (operator.equals("^")) {
+        answer = exponential(inputs[0], inputs[1]);
+    }
+    
+    if (operator.equals("1/x")) {
+        answer = inverseOfNumber(inputs[0]);
+    }
+    
+    if (operator.equals("+or-")) {
+        answer = positiveOrNegative(inputs[0]);
+    
 }
+    System.out.println(answer);
+}
+    
+    public static void getInputs(Scanner scanner) {
+        for (int i = 0; i < numberOfInputs; i++){
+        System.out.println("Please enter next input");
+        inputs[i] = scanner.nextDouble();
+    }
+    }   
+    
+    public static double sum(double input1, double input2){
+        return input1 + input2;
+    }
+    
+    public static double difference(double input1, double input2){
+        return input1 - input2;
+    }
+    public static double product(double input1, double input2){
+        return input1 * input2;
+    }
+    
+    public static double division(double input1, double input2){
+        if (input2==0)
+            
+            return Double.parseDouble(s);
+        else
+        return input1 / input2;
+    }
+    public static double square(double input1){
+        return Math.pow(input1,2);
+    }
+    
+    public static double squareroot(double input1) {
+        return  Math.sqrt(input1);
+    }
+    public static double exponential(double input1, double input2){
+        return Math.pow(input1,input2);
+    }
+   public static double inverseOfNumber(double input1)
+   {
+        return 1/input1;
+    }
+    
+    public static double positiveOrNegative(double input1)
+   {
+        return input1*=-1;
+    }
+    /**
+     * Constructor for objects of class IOConsole
+     */
+    
+
+    /**
+     * An example of a method - replace this comment with your own
+     *
+     * @param  y  a sample parameter for a method
+     * @return    the sum of x and y
+     */
+    
+}
+
+    
+    
---
+++ MainApplicationTest.java
@@ -0,0 +1,138 @@
+
+
+import static org.junit.Assert.*;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.Assert;
+
+/**
+ * The test class MainApplicationTest.
+ *
+ * @author  (your name)
+ * @version (a version number or a date)
+ */
+public class MainApplicationTest
+{
+    private MainApplication mainApplication;
+    
+    @Before
+    public void setUp()
+    {MainApplication mainApplication = new MainApplication();
+    }
+    
+    @Test
+    public void sumTest()
+    { double expected = 31.0;
+        double a = 15.0;
+        double b = 16.0;
+       
+      double actual = mainApplication.sum(a, b);
+        Assert.assertEquals("Values should be equal", expected, actual, 0.0);
+    
+        
+    }
+    
+    @Test
+    public void differenceTest()
+    { double expected = 11.0;
+        double a = 22.0;
+        double b = 11.0;
+       
+      double actual = mainApplication.difference(a, b);
+        Assert.assertEquals("Values should be equal", expected, actual, 0.0);
+    
+        
+    }
+    
+    @Test
+    public void productTest()
+    { double expected = 30.0;
+        double a = 5.0;
+        double b = 6.0;
+       
+      double actual = mainApplication.product(a, b);
+        Assert.assertEquals("Values should be equal", expected, actual, 0.0);
+    
+        
+    }
+    
+    @Test
+    public void division_1Test()
+    { double expected = 31.0;
+        double a = 28.0;
+        double b = 7.0;
+       
+      double actual = mainApplication.division(a, b);
+        Assert.assertEquals("Values should be equal", expected, actual, 0.0);
+    
+        
+    }
+    
+    @Test
+    public void division_2Test(){
+        String s = "Error";
+        double expected = Double.parseDouble(s);
+        double a = 28.0;
+        double b = 0.0;
+        double actual = mainApplication.division(a,b); 
+        Assert.assertEquals("Values should be equal", expected, actual, 0.0);
+        Assert.assertEquals("Value should not be equal", expected, actual);
+        
+  } 
+      
+    
+    @Test
+    public void squareTest()
+    { double expected = 49.0;
+        double a = 7.0;
+      double actual = mainApplication.square(a);
+        Assert.assertEquals("Values should be equal", expected, actual, 0.0);    
+    }
+    
+    @Test
+    public void squarerootTest()
+    { double expected = 9.0;
+        double a = 81.0;
+      double actual = mainApplication.squareroot(a);
+        Assert.assertEquals("Values should be equal", expected, actual, 0.0);
+    
+        
+    }
+    
+    @Test
+    public void exponentialTest()
+    { double expected = 343.0;
+        double a = 7.0;
+        double b = 3.0;
+      double actual = mainApplication.exponential(a, b);
+        Assert.assertEquals("Values should be equal", expected, actual, 0.0);
+    
+        
+    }
+    
+    @Test
+    public void inverseOfNumber()
+    { double expected = 1/52.0;
+        double a = 52.0;
+      double actual = mainApplication.inverseOfNumber(a);
+        Assert.assertEquals("Values should be equal", expected, actual, 0.0);
+    }
+    
+    
+     @Test
+    public void positiveOrNegative()
+    { double expected = -18.0;
+        double a = 18.0;
+      double actual = mainApplication.positiveOrNegative(a);
+        Assert.assertEquals("Values should be equal", expected, actual, 0.0);
+        
+    }
+    
+    
+
+    @After
+    public void tearDown()
+    {
+    }
+}
